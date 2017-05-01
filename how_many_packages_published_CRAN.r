2911c52071f384e6c606697502efb56e27842dc3
HI\pedroc <pedro.concejerocerezo@telefonica.com>
2017-05-01 19:52:40 +0200

    new script

---
+++ how_many_packages_published_CRAN.r
@@ -0,0 +1,28 @@
+
+# How many packages are published on CRAN
+# From here:
+# http://blog.revolutionanalytics.com/2015/06/how-many-packages-are-there-really-on-cran.html
+
+types <- c("source", "win.binary", 
+           "mac.binary", "mac.binary.mavericks")
+
+CRANmirror <- "http://cran.revolutionanalytics.com"
+
+pdb <- lapply(types, function(x){
+  cran <- contrib.url(repos = CRANmirror, 
+                      type = x)
+  available.packages(contriburl = cran, type = x)
+})
+names(pdb) <- types
+str(pdb, max.level = 1)
+
+
+# Number of available packages
+sapply(pdb, nrow)
+
+# Display first few packages for each type
+pkgs <- sapply(pdb, rownames)
+lapply(pkgs, head)
+
+# Create list of differences between source and win.binary
+setdiff(pkgs[["source"]], pkgs[["win.binary"]])
