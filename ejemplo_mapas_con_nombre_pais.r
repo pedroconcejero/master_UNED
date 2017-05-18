25e3a92a64c57b4d94f4db18dcb5724ca83f11f9
HI\pedroc <pedro.concejerocerezo@telefonica.com>
2017-05-18 11:34:31 +0200

    subo ejemplito mapas

---
+++ ejemplo_mapas_con_nombre_pais.r
@@ -0,0 +1,15 @@
+
+install.packages(c("maps", "mapdata"))
+library(maps)
+library(mapdata)
+
+
+map('worldHires',
+    c('Spain', 'Portugal'))
+
+map('worldHires',
+    c('UK', 'Ireland', 'Isle of Man','Isle of Wight', 'Wales:Anglesey'))
+
+map('worldHires',
+    c('UK', 'Ireland', 'Isle of Man','Isle of Wight'),
+    xlim=c(-11,3), ylim=c(49,60.9))	
